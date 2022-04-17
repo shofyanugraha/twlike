133e8d5d3aa417fcdc6dc3dca1cdcda6a16a2554
rizkysalt <102502166+rizkysalt@users.noreply.github.com>
2022-04-17 17:33:24 +0700

    fix: build

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RouterViewTransition from '@/components/RouterViewTransition.vue'
+</script>
 
 <template>
   <RouterViewTransition is-root></RouterViewTransition>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import RouterViewTransition from '@/components/RouterViewTransition.vue'
+</script>
+
 <template>
   <RouterViewTransition></RouterViewTransition>
 </template>
